8daa27ae8e1c62a26866394d1eb0d9fe93eb6b7f
Parathan Thiyagalingam <parathanlive123@gmail.com>
2022-07-07 14:46:34 +0530

    script

--- script.sh
+++ script.sh
@@ -10,6 +10,6 @@ docker --version
 
 sudo docker ps
 
-sudo docker image build -t flask_docker .
+#sudo docker image build -t flask_docker .
 
-sudo  docker run -p 80:3001 flask_docker
+#sudo  docker run -p 80:3001 flask_docker
