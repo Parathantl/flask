c784a1aa38ede3345dffd40a7a26363765efd2ac
Parathan Thiyagalingam <parathanlive123@gmail.com>
2022-07-07 22:35:39 +0530

    changes

--- python/app.py
+++ python/app.py
@@ -1,14 +1,14 @@
 from haystack.nodes import DensePassageRetriever
 from haystack.document_stores import InMemoryDocumentStore
 
-doc_dir = "/home/ubuntu/flask/content"
+doc_dir = "/home/ubuntu/flask/python/content"
 train_filename = "answersDPR.json"
 dev_filename = "answersDPR.json"
 
 query_model = "facebook/dpr-question_encoder-single-nq-base"
 passage_model = "facebook/dpr-ctx_encoder-single-nq-base"
 
-save_dir = "/home/ubuntu/flask/saved_models"
+save_dir = "/home/ubuntu/flask/python/saved_models"
 
 ## Initialize DPR model
 
