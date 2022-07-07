189bb9fe92afc3b83e9d06b2872acad64f648ceb
Parathan Thiyagalingam <parathanlive123@gmail.com>
2022-07-07 20:17:28 +0530

    script

--- api/routes/query.py
+++ api/routes/query.py
@@ -11,6 +11,10 @@ from haystack.pipelines import GenerativeQAPipeline
 from haystack.document_stores import ElasticsearchDocumentStore
 import shutil
 
+from haystack.utils import launch_es
+
+launch_es()
+
 query_routes = Blueprint("query", __name__, url_prefix="/api/query")
 
 if os.path.exists("./api/routes/data/"):
