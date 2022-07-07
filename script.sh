5ea757adcb30faa4ccd8750445dca50e9d845b4d
Parathan Thiyagalingam <parathanlive123@gmail.com>
2022-07-07 11:22:43 +0530

    first commit

---
+++ script.sh
@@ -0,0 +1,11 @@
+sudo apt-get remove docker docker-engine docker.io
+
+sudo apt-get remove docker docker-engine docker.io
+
+sudo apt install docker.io
+
+sudo snap install docker
+
+docker --version
+
+sudo docker ps
