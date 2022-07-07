beec8b58bba4df63df07d10755b43e67ed1739c7
Parathan Thiyagalingam <parathanlive123@gmail.com>
2022-07-08 00:38:51 +0530

    last

--- python/view.py
+++ python/view.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 from datetime import timedelta
-import os
 
 from flask import Flask
 from flask.helpers import send_from_directory
@@ -27,7 +26,15 @@ from api.routes.status import status_routes
 from api.routes.courses import course_routes
 from api.routes.mentors import mentor_routes
 from api.routes.companies import company_routes
-from api.routes.query import query_routes
+
+from haystack.nodes import DensePassageRetriever
+from haystack.utils import fetch_archive_from_http
+from haystack.utils import convert_files_to_docs, fetch_archive_from_http, clean_wiki_text
+from haystack.nodes import Seq2SeqGenerator
+
+from haystack.document_stores import FAISSDocumentStore
+from haystack.pipelines import GenerativeQAPipeline
+from haystack.document_stores import ElasticsearchDocumentStore
 
 from dotenv import load_dotenv
 
@@ -81,12 +88,46 @@ app.register_blueprint(status_routes)
 app.register_blueprint(course_routes)
 app.register_blueprint(company_routes)
 app.register_blueprint(mentor_routes)
-app.register_blueprint(query_routes)
+
+save_dir = "./saved_models"
+
+# document_store_1 = FAISSDocumentStore(faiss_index_factory_str="Flat", similarity="dot_product")
+
+doc_store = ElasticsearchDocumentStore(host="localhost", username="", password="", index="document")
+
+# Let's first get some files that we want to use
+docu_dir = "./api/routes/data/tutorial12"
+s3_url = "https://bitbucket.org/parathant/rp-project/raw/bc3925e7dc8d5e5925b9be2668ba0438506e3a95/dataset.zip"
+fetch_archive_from_http(url=s3_url, output_dir=docu_dir)
+
+# Convert files to dicts
+docs = convert_files_to_docs(dir_path=docu_dir, clean_func=clean_wiki_text, split_paragraphs=True)
+
+# Now, let's write the dicts containing documents to our DB.
+doc_store.write_documents(docs)
+
+reloaded_retriever = DensePassageRetriever.load(load_dir=save_dir, document_store=doc_store)
+
+doc_store.update_embeddings(reloaded_retriever)
+
+# Reader/Generator
+generator = Seq2SeqGenerator(model_name_or_path="vblagoje/bart_lfqa")
+
+pipe = GenerativeQAPipeline(generator, reloaded_retriever)
 
 @app.route('/')
 def home():
     return {"hello": "world"}
 
+@app.route('/api/query', methods=['POST'])
+def getQuery():
+    form_data = request.get_json()['results']
+
+    query = form_data['query']
+
+    pipe.run(query=query, params={"Retriever": {"top_k": 5}})
+
+    return 0
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 3001))
