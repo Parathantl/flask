0ddac07eb601bf7b631deb139dc4b3a09ff49796
Parathan Thiyagalingam <parathanlive123@gmail.com>
2022-07-09 05:03:15 +0530

    add

--- python/script.sh
+++ python/script.sh
@@ -11,5 +11,10 @@ docker --version
 sudo docker ps
 
 #sudo docker image build -t flask-python .
-
 #sudo  docker run -p 80:80 --net=host flask-python
+
+
+# docker volume create elast_data
+# sudo docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -v elast_data:/usr/share/elasticsearch/data docker.elastic.co/elasticsearch/elasticsearch:7.16.3
+
+# curl -X GET "localhost:9200/"
